fd1b18a4885d70a4cc74946ae8e91ccaf578c612
Jason Hurley <paperjace@gmail.com>
2025-03-25 12:22:31 -0400

    changes dir structure

--- flake.nix
+++ flake.nix
@@ -12,12 +12,12 @@
       nixos = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
         modules = [
-          ./configuration.nix
+          ./system/configuration.nix
           home-manager.nixosModules.home-manager
           {
             home-manager.useGlobalPkgs = true;
             home-manager.useUserPackages = true;
-            home-manager.users.jason = import ./home.nix;
+            home-manager.users.jason = import ./home/home.nix;
 
             # Optionally, use home-manager.extraSpecialArgs to pass
             # arguments to home.nix
